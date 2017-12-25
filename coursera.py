500d348e69c9ef12db4484a66046324e54245d2e
kheeva <kheeva@ya.ru>
2017-12-25 18:49:27 +0300

    all issues were fixed

--- coursera.py
+++ coursera.py
@@ -7,7 +7,7 @@ from bs4 import BeautifulSoup
 from openpyxl import Workbook
 
 
-def get_list_of_20_random_courses():
+def get_random_courses_list(courses_amount):
     url = 'https://www.coursera.org/sitemap~www~courses.xml'
     courses_list = []
     root = ElementTree.fromstring(requests.get(url).text)
@@ -16,24 +16,17 @@ def get_list_of_20_random_courses():
     for elem in root.findall('ns:url', namespace_map):
         courses_list.append(elem.find('ns:loc', namespace_map).text)
 
-    courses_limit = 20
     start_index = 0
-    random_course = randint(start_index, len(courses_list) - courses_limit)
-    return courses_list[random_course: random_course + courses_limit]
+    random_course = randint(start_index, len(courses_list) - courses_amount)
+    return courses_list[random_course: random_course + courses_amount]
 
 
-def get_course_info(course__url):
-    request = requests.get(course__url)
-    request.encoding = 'utf-8'
-    soup = BeautifulSoup(request.text, 'html.parser')
-
+def get_course_info(soup):
     def get_course_attribute(html_tag, html_class):
         try:
-            course_attribute = soup.find(html_tag, class_=html_class).text
+            return soup.find(html_tag, class_=html_class).text
         except AttributeError:
             return None
-        else:
-            return course_attribute
 
     course_name = get_course_attribute('h1', 'title display-3-text')
     course_language = get_course_attribute('div', 'rc-Language')
@@ -45,7 +38,7 @@ def get_course_info(course__url):
     try:
         course_weeks_number = len(soup.findAll('div', class_='week'))
     except AttributeError:
-        return None
+        course_weeks_number = None
 
     return {'name': course_name,
             'language': course_language,
@@ -65,16 +58,14 @@ def output_courses_info_to_xlsx(file_path, courses):
     worksheet['E1'] = 'Weeks'
     worksheet['F1'] = 'Score'
 
-    offset = 2
-    for row, course in enumerate(courses):
-        row_number = row + offset
-        worksheet['A{}'.format(row_number)] = course
-        worksheet['B{}'.format(row_number)] = courses[course]['name']
-        worksheet['C{}'.format(row_number)] = courses[course]['language']
-        worksheet['D{}'.format(row_number)] = courses[course]['start_date']
-        worksheet['E{}'.format(row_number)] = courses[course][
+    for row, course in enumerate(courses, start=2):
+        worksheet['A{}'.format(row)] = course
+        worksheet['B{}'.format(row)] = courses[course]['name']
+        worksheet['C{}'.format(row)] = courses[course]['language']
+        worksheet['D{}'.format(row)] = courses[course]['start_date']
+        worksheet['E{}'.format(row)] = courses[course][
             'number_of_weeks']
-        worksheet['F{}'.format(row_number)] = courses[course]['score']
+        worksheet['F{}'.format(row)] = courses[course]['score'] or 'not rated'
 
     workbook.save(file_path)
 
@@ -85,11 +76,14 @@ if __name__ == '__main__':
 
     courses_dict = {}
     try:
-        courses_urls_list = get_list_of_20_random_courses()
+        courses_urls_list = get_random_courses_list(courses_amount=20)
     except ConnectionResetError:
         exit('Could\'t connect to coursera. Try again later.')
     else:
         for course_url in courses_urls_list:
-            courses_dict[course_url] = get_course_info(course_url)
+            request = requests.get(course_url)
+            request.encoding = 'utf-8'
+            course_soup = BeautifulSoup(request.text, 'html.parser')
+            courses_dict[course_url] = get_course_info(course_soup)
 
         output_courses_info_to_xlsx(sys.argv[1], courses_dict)
